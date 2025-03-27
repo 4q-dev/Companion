7092794cb186569ce3c868743e1025257621130e
Dmitry Hudrich <dimahudric@gmail.com>
2025-03-27 16:02:47 +0300

    init

---
+++ LoggingConfigure.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+
+internal static class LoggingConfigure {
+    public static void ConfigureLogging() {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("logs/log.zaza",
+                rollingInterval: RollingInterval.Day,
+                rollOnFileSizeLimit: true)
+            .CreateLogger();
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Serilog;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
+LoggingConfigure.ConfigureLogging();
+
 using var cts = new CancellationTokenSource();
+
 var bot = new TelegramBotClient(Environment.GetEnvironmentVariable("ZAZAGRAM_TOKEN")!, cancellationToken: cts.Token);
 var me = await bot.GetMe();
-bot.OnMessage += OnMessage;
 
-Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
-Console.ReadLine();
-cts.Cancel();
-
-async Task OnMessage(Message msg, UpdateType type) {
+bot.OnMessage += async (Message msg, UpdateType type) => {
     if (msg.Text is null) return;
-    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+    Log.Information($"Received {type} '{msg.Text}' in {msg.Chat}");
     await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
-}
+};
+
+Log.Information($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+
+cts.Cancel();
